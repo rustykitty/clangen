091c1a8e3272dc9f49f3d31ff85c0845cafe0853
bingle <136284894+dinkwe@users.noreply.github.com>
2025-02-07 18:49:56 -0800

    Update ProfileScreen.py

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -2399,10 +2399,11 @@ class ProfileScreen(Screens):
 
         x_pos = 30
         for con in all_illness_injuries[self.conditions_page]:
-            if self.the_cat.permanent_condition[con[0]]["misdiagnosis"] and self.the_cat.permanent_condition[con[0]]["misdiagnosis"] is not False: 
-                condition_name = self.change_condition_name(con[0], self.the_cat.permanent_condition[con[0]]["misdiagnosis"])
-            else:
-                condition_name = self.change_condition_name(con[0])
+            if con[0] in self.the_cat.permanent_condition:
+                if self.the_cat.permanent_condition[con[0]]["misdiagnosis"] and self.the_cat.permanent_condition[con[0]]["misdiagnosis"] is not False: 
+                    condition_name = self.change_condition_name(con[0], self.the_cat.permanent_condition[con[0]]["misdiagnosis"])
+                else:
+                    condition_name = self.change_condition_name(con[0])
 
             # Background Box
             pygame_gui.elements.UIImage(
