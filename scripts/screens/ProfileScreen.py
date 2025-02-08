f25b69e299191488a8db9887603edd2a42ca8988
bingle <136284894+dinkwe@users.noreply.github.com>
2025-02-07 19:04:23 -0800

    Update ProfileScreen.py

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -2400,7 +2400,7 @@ class ProfileScreen(Screens):
         x_pos = 30
         for con in all_illness_injuries[self.conditions_page]:
             if con[0] in self.the_cat.permanent_condition:
-                if self.the_cat.permanent_condition[con[0]]["misdiagnosis"] and self.the_cat.permanent_condition[con[0]]["misdiagnosis"] is not False: 
+                if "misdiagnosis" in self.the_cat.permanent_condition[con[0]] and self.the_cat.permanent_condition[con[0]]["misdiagnosis"] is not False: 
                     condition_name = self.change_condition_name(con[0], self.the_cat.permanent_condition[con[0]]["misdiagnosis"])
                 else:
                     condition_name = self.change_condition_name(con[0])
