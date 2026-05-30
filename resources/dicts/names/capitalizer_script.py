5730df12fe8d6a68fe1d0934d0d921fe2f21f626
Rusty <rusty3925@gmail.com>
2026-05-30 09:03:39 -0700

    capitalize suffixes in names.json

---
+++ resources/dicts/names/capitalizer_script.py
@@ -0,0 +1,28 @@
+import json
+
+import os
+import os.path
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+names: dict[str, dict[str, list[str]] | list[str]]
+
+with open("./names.json") as fp:
+    names = json.load(fp)
+
+# iterate through all suffixes and capitalize them
+for key in names:
+    if key.endswith("_suffixes"):
+        sub = names[key]
+        if isinstance(sub, list):
+            sub = [
+                s.capitalize() for s in sub
+            ]
+        elif isinstance(sub, dict):
+            sub = {
+                k: [
+                    s.capitalize() for s in v
+                ] for (k, v) in sub.items()
+            }
+
+with open("./names.json", "w") as fp:
+    json.dump(names, fp, indent=2)
