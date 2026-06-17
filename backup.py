5117c764177a17d7188fa90ad55c0da6e62f5bb3
Rusty <rusty3925@gmail.com>
2026-06-17 15:09:13 -0700

    backup save, restore, and list working from cli

--- backup.py
+++ backup.py
@@ -1,9 +1,13 @@
 import shutil
 import datetime
+import glob
+import sys
+import os
+import os.path
 
 import logging
 
-from scripts.housekeeping.datadir import get_save_dir, get_backups_dir
+from scripts.housekeeping.datadir import get_save_dir, get_backups_dir, get_temp_dir
 
 logger = logging.getLogger(__name__)
 
@@ -18,11 +22,45 @@ def make_backup(name):
         logger=logger,
     )
 
+def restore_backup(name, timestamp):
+    backup_path = get_backups_dir() + f"/{timestamp}_{name}.zip"
+    clan_path = get_save_dir() + f"/{name}"
+    temp = get_temp_dir() + "/backup_restore"
+    shutil.unpack_archive(backup_path, temp, "zip")
+
+    # delete the old clan path
+    if os.path.exists(clan_path):
+        shutil.rmtree(clan_path)
+    shutil.copytree(temp, clan_path)
+
+def list_backups(name):
+    files = glob.glob(get_backups_dir() + f"/*_{name}.zip")
+    return [file.lstrip(get_backups_dir() + "/") for file in files]
+
+USAGE = "Usage: backup.py <Clan name> <backup|restore> [options...]"
+
+def main():
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print(USAGE)
+        return
+    clan = args[0]
+    if len(args) >= 2:
+        subcommand = args[1]
+        if subcommand == "backup":
+            make_backup(clan)
+        elif subcommand == "restore":
+            if len(args) == 3:
+                restore_backup(args[0], args[2])
+            else:
+                print(USAGE)
+        else:
+            print(USAGE)
+    else: # 1 arg
+        backups = list_backups(clan)
+        print("Backups for", clan, f"({len(backups)})")
+        for backup in backups:
+            print(f"-", backup)
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) == 2:
-        make_backup(sys.argv[1])
-    else:
-        print("Usage: backup.py <Clan name>")
+    main()
